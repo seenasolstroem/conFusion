2f1bcf77a56085afdf3ccfd5a08419babf67a423
seenasolstroem <sc@solstroem.com>
2022-02-22 15:08:24 +0100

    last

--- app.js
+++ app.js
@@ -1,86 +1,126 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-let dishRouter = require('./routes/dishRouter');
-let leaderRouter = require('./routes/leaderRouter');
-let promotionsRouter= require('./routes/promotionsRouter');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+let dishRouter = require("./routes/dishRouter");
+let leaderRouter = require("./routes/leaderRouter");
+let promotionsRouter = require("./routes/promotionsRouter");
 
-const mongose =require('mongoose')
-const Dishes=require('./models/dishes');
-const mongoose = require('mongoose');
-const url= 'mongodb://localhost:27017/conFusion';
-const connect=mongoose.connect(url);
+const mongose = require("mongoose");
+const Dishes = require("./models/dishes");
+const mongoose = require("mongoose");
+const url = "mongodb://localhost:27017/conFusion";
+const connect = mongoose.connect(url);
 
-connect.then((db)=>{
-  console.log("connected correctly to the server");
-},(err)=>{console.log(err);})
+connect.then(
+  (db) => {
+    console.log("connected correctly to the server");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser("12345-67890-09876-54321"));
+app.use(express.static(path.join(__dirname, "public")));
 
-function auth(req,res,next){
-  console.log(req.headers);
-
-  const authHeader =req.headers.authorization;
-  if(!autherHeader){
-    const err=new Error('you are not authenticated!')
-    res.setHeader( 'WWW-Authenticate','Basic');
-    err.status=401;
-    return next(err);
+function auth(req, res, next) {
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+      var err = new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      next(err);
+      return;
+    }
+    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+      .toString()
+      .split(":");
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == "admin" && pass == "password") {
+      res.cookie("user", "admin", { signed: true });
+      next(); // authorized
+    } else {
+      var err = new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      next(err);
+    }
+  } else {
+    if (req.signedCookies.user === "admin") {
+      next();
+    } else {
+      var err = new Error("You are not authenticated!");
+      err.status = 401;
+      next(err);
+    }
   }
-  const auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-  const username=auth[0];
-  const password=auth[1];
-
-   if(username==='admin' && password === 'password'){
-     next();
-   }
-   else{
-     const err=new Error('you are not authenticated!')
-    res.setHeader( 'WWW-Authenticate','Basic');
-    err.status=401;
-    return next(err);
-   }
-
 }
 
+// function auth(req,res,next){
+//   console.log(req.headers);
+
+//   const authHeader =req.headers.authorization;
+//   if(!autherHeader){
+//     const err=new Error('you are not authenticated!')
+//     res.setHeader( 'WWW-Authenticate','Basic');
+//     err.status=401;
+//     return next(err);
+//   }
+//   const auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+//   const username=auth[0];
+//   const password=auth[1];
+
+//    if(username==='admin' && password === 'password'){
+//      next();
+//    }
+//    else{
+//      const err=new Error('you are not authenticated!')
+//     res.setHeader( 'WWW-Authenticate','Basic');
+//     err.status=401;
+//     return next(err);
+//    }
+
+// }
 
 app.use(auth);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/dishes',dishRouter);
-app.use('/promotions',promotionsRouter);
-app.use('/leaders',leaderRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/dishes", dishRouter);
+app.use("/promotions", promotionsRouter);
+app.use("/leaders", leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
